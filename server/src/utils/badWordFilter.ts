50a030cafc2d33315adb906b05852cf11c1a91df
J <65231837+ItsStorm123@users.noreply.github.com>
2024-03-21 17:20:38 +0800

    feet: add chink to badwordfilter

--- server/src/utils/badWordFilter.ts
+++ server/src/utils/badWordFilter.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-multi-spaces */
 const badWordRegexes: RegExp[] = [
-    /n[i1l!]+[g693]+[e3]*[ra@]*/i,    // N word
-    /f[a@4]+[g9]+[s$5z2]+[o0]+[t+]*/i // F slur
+    /n[i1l!]+[g693]+[e3]*[ra@]*/i,    // nigger
+    /f[a@4]+[g9]+[s$5z2]+[o0]+[t+]*/i, // faggot
+    /c[h]+[i1l!]+[n]+[k]*/i, // chink
 ];
 
 export function hasBadWords(text: string): boolean {
